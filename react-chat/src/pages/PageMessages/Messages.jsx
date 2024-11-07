f58677cb15b67e9916471e6cd975145b0e358073
margarem <margarem@student.21-school.ru>
2024-11-08 01:19:57 +0300

    messagesItem

--- react-chat/src/pages/PageMessages/Messages.jsx
+++ react-chat/src/pages/PageMessages/Messages.jsx
@@ -1,19 +1,19 @@
-import React, { useState, useRef, useEffect } from "react";
-import { usePageContext } from '../../ExportModule/PageContext.jsx';
+import React, {useState, useRef, useEffect} from "react";
+import {usePageContext} from '../../ExportModule/PageContext.jsx';
 import Menu from "./Menu";
 import styles from '../../styles/messagesPage.module.scss';
 import Header from './Header.jsx';
 import {useTheme} from '../../ExportModule/ThemeContext.jsx'; // Import utility functions
-import { getChatName, getRelativeDate, getCurrentUserId } from "../../ExportModule/utils.js";
+import {getChatName, getRelativeDate, getCurrentUserId} from "../../ExportModule/utils.js";
 import {chatData} from "../../ExportModule/classes/chat/chats.js";
-import { IconButton } from '@mui/material'; // Import Material UI components
+import {IconButton} from '@mui/material'; // Import Material UI components
 import DeleteIcon from '@mui/icons-material/Delete';
 
 const Messages = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     const menuRef = useRef(null);
-    const { navigateTo } = usePageContext();
-    const { isDarkMode} = useTheme();
+    const {navigateTo} = usePageContext();
+    const {isDarkMode} = useTheme();
     const [userChats, setUserChats] = useState([]);
 
     const toggleMenu = () => {
@@ -36,7 +36,6 @@ const Messages = () => {
         // Fetch user chats when component mounts
         const currentUserId = getCurrentUserId(); // Assuming this function is available
         const chats = chatData.getChatsForUser(currentUserId);
-        alert(currentUserId);
         setUserChats(chats);
     }, []);
 
@@ -54,38 +53,39 @@ const Messages = () => {
     };
 
     return (
-        <div className={`${styles.messagesPage} ${isDarkMode ? styles.darkMode : ''}`}  onClick={handleDocumentClick}>
+        <div className={`${styles.messagesPage} ${isDarkMode ? styles.darkMode : ''}`} onClick={handleDocumentClick}>
             <Header toggleMenu={toggleMenu} navigateTo={navigateTo}/>
             <main className={styles.messagesMain}>
                 {/* Основной контент сообщений */}
-                <ul className={styles.chatsList}>
-                    {userChats.length > 0 ? userChats.map(chat => {
-                        const lastMessage = chat.messages[chat.messages.length - 1] || {};
-                        const lastMessageText = lastMessage.text || 'Нет сообщений';
-                        const relativeDate = getRelativeDate(lastMessage.date);
-                        const chatName = getChatName(chat);
-
-                        return (
-                            <li key={chat.id} className={styles.chatItem} onClick={() => handleChatClick(chat.id)}>
-                                <div className={styles.chatInfo}>
-                                    <div className={styles.chatName}>{chatName}</div>
-                                    <div className={styles.chatLastMessage}>{lastMessageText}</div>
-                                </div>
-                                <div className={styles.chatTimestamp}>
-                                    <div>{relativeDate}</div>
-                                    <IconButton onClick={(e) => {
-                                        e.stopPropagation();
-                                        handleDeleteChat(chat.id);
-                                    }}>
-                                        <DeleteIcon/>
-                                    </IconButton>
-                                </div>
-                            </li>
-                        );
-                    }) : (
-                        <p>У вас нет чатов</p>
-                    )}
-                </ul>
+                <div className={styles.chatsContainer}>
+                    <ul className={styles.chats}>
+                        {userChats.length > 0 ? userChats.map(chat => {
+                            const lastMessage = chat.messages[chat.messages.length - 1] || {};
+                            const lastMessageText = lastMessage.text || 'Нет сообщений';
+                            const relativeDate = getRelativeDate(lastMessage.date);
+                            const chatName = getChatName(chat);
+                            return (
+                                <li key={chat.id} className={styles.chat} onClick={() => handleChatClick(chat.id)}>
+                                    <div className={styles.chatInfo}>
+                                        <div className={styles.chatName}>{chatName}</div>
+                                        <div className={styles.chatLastMessage}>{lastMessageText}</div>
+                                    </div>
+                                    <div className={styles.chatTimestamp}>
+                                        <div className={styles.time}>{relativeDate}</div>
+                                        <IconButton className={styles.deleteButton} onClick={(e) => {
+                                            e.stopPropagation();
+                                            handleDeleteChat(chat.id);
+                                        }}>
+                                            <DeleteIcon className={styles.deleteButtonIco}/>
+                                        </IconButton>
+                                    </div>
+                                </li>
+                            );
+                        }) : (
+                            <p>У вас нет чатов</p>
+                        )}
+                    </ul>
+                </div>
             </main>
             <Menu
                 ref={menuRef}
